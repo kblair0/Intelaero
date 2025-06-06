5ef4a908f464b0254f1cdacf798fb7ba607d3c87
KBA <kjdblair89@gmail.com>
2025-06-07 08:20:40 +1000

    Moved all flight-assurance files to intelaero root

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/kieranblair/git/fa/flight-assurance/src/app/layout\":[\"static/media/f28c0d0ddfdf7c25-s.p.woff2\",\"static/media/f884d4ea94220255-s.p.woff2\",\"static/media/e76854c588de135c-s.p.woff2\",\"static/media/e94cfb95acf534f2-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
